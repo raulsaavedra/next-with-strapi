d7248c1a0a1cc39784f7d8a3a9e5bffd3c95d28e
raulsaavedra <raul670saavedra@gmail.com>
2021-05-18 09:17:59 -0400

    feat: babel plugin and strapi custom

--- backend/api/article/config/schema.graphql.js
+++ backend/api/article/config/schema.graphql.js
@@ -4,20 +4,36 @@ module.exports = {
   query: `
     articleBySlug(slug: String!): Article
   `,
+  mutation: `
+    increaseLikes(slug: String!): Article
+  `,
   resolver: {
     Query: {
       articleBySlug: {
-        description: 'Return the article by category',
+        description: 'Return the article by slug',
         resolverOf: 'Article.findOne', 
         resolver: async (obj, {slug}, ctx) => {
           // ctx is the context of the Koa request.
           
           const entity = await strapi.query('article').findOne({slug})
 
-          console.log(entity)
           return sanitizeEntity(entity, { model: strapi.models.article });
         }
       },
     },
+    Mutation: {
+      increaseLikes: {
+        description: 'Increase article likes',
+        resolverOf: 'Article.update',
+         resolver: async (obj, {slug}, ctx) => {
+          // ctx is the context of the Koa request.
+          
+          const entity = await strapi.query('article').findOne({slug})
+          const entityLikes = entity.likes;
+          const newEntity = await strapi.query('article').update({ slug }, { ...entity, likes: parseInt(entityLikes || 0) + 1 })
+          return sanitizeEntity(newEntity, { model: strapi.models.article });
+        }
+      }
+    }
   },
 };
