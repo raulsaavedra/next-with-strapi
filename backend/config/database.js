f17d0147bf382d5d479fb43a615d2b06e87956c6
raulsaavedra <raul670saavedra@gmail.com>
2021-05-25 06:38:19 -0400

    feat: with postgres and fixes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,22 @@
+require('dotenv').config();
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+        port: env('DATABASE_PORT', process.env.DATABASE_PORT),
+        database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+        username: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+        password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {
-        useNullAsDefault: true,
-      },
+      options: {},
     },
   },
 });
